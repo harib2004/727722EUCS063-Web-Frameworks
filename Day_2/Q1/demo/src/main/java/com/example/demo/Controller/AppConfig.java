24bc480a74d6186ceed95ff86e53dff6536b1d1e
harib2004 <haribaskar0077@gmail.com>
2024-04-05 15:21:06 +0530

    Revert "second commit"
    This reverts commit 3545695ba9139a1bcec37556ea9a5f58bad50a17.

--- Day_2/Q1/demo/src/main/java/com/example/demo/Controller/AppConfig.java
+++ Day_2/Q1/demo/src/main/java/com/example/demo/Controller/AppConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo.Config;
+package com.example.demo.Controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
