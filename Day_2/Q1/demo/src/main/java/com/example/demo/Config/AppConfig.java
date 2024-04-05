075521207db1ef4cb7b58b955dbcefedee2ed8e1
harib2004 <haribaskar0077@gmail.com>
2024-04-05 15:28:38 +0530

    Revert "Revert "second commit""
    This reverts commit 24bc480a74d6186ceed95ff86e53dff6536b1d1e.

--- Day_2/Q1/demo/src/main/java/com/example/demo/Config/AppConfig.java
+++ Day_2/Q1/demo/src/main/java/com/example/demo/Config/AppConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo.Controller;
+package com.example.demo.Config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
--- Day_2/Q1/demo/src/main/java/com/example/demo/Controller/ApiController.java
+++ Day_2/Q1/demo/src/main/java/com/example/demo/Controller/ApiController.java
@@ -1,10 +1,10 @@
 package com.example.demo.Controller;
 
-import com.example.demo.Controller.AppConfig;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Config.AppConfig;
 
 @RestController
 public class ApiController {
