40915eca5cb2ffc04e062b5062cc30eb65c949ee
harib2004 <haribaskar0077@gmail.com>
2024-04-24 14:41:02 +0530

    seventh commit

--- Haribaskar_Day_11/Haribaskar_CW1/src/main/java/com/example/haribaskarcw1/Haribaskarcw1Application.java
+++ Haribaskar_Day_11/Haribaskar_CW1/src/main/java/com/example/haribaskarcw1/Haribaskarcw1Application.java
@@ -1,4 +1,4 @@
-package com.example.bidirection;
+package com.example.haribaskarcw1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
